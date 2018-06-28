8b1a074bd78bcab8f7345cb192765944b34fd79e
Rashad Hammonds <rhammonds@yearup.org>
2018-06-28 12:01:15 -0400

    added Auth for soundcloud.

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -15,6 +15,7 @@ function Stream({ tracks = [] }) {
 
 //identify redux as a supplier for data for use. while the component doesn't change at all.
 //mapStateToProps returns a substate of our global scope supplying only the data this component needs
+//we can access different properties of the global scope using <Stream something={thing} />
 function mapStatetoProps(state) {
     const tracks = state.track;
     return {
---
+++ src/constants/auth.js
@@ -0,0 +1,2 @@
+export const CLIENT_ID = 'db1a9cf92ac128e893bad0c79db66245';
+export const REDIRECT_URI = `${window.location.protocol}//${window.location.host}/callback`;
